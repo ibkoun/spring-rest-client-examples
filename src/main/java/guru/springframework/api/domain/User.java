43b5dcde71f14e87168db429c1ddb00d6bfb0d33
ibkoun <idole.koun@outlook.com>
2021-04-14 10:14:42 -0400

    Added POJOs generated from https://www.jsonschema2pojo.org/

---
+++ src/main/java/guru/springframework/api/domain/User.java
@@ -0,0 +1,34 @@
+
+package guru.springframework.api.domain;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+
+@Generated("jsonschema2pojo")
+public class User implements Serializable
+{
+
+    private List<Datum> data = null;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = -3337109495300817355L;
+
+    public List<Datum> getData() {
+        return data;
+    }
+
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
